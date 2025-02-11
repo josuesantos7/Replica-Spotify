ea6329e7716772daf8c3c9352c2a5a06a17c55b3
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2025-02-10 21:39:41 -0300

    Adicionado o Header e estilizações.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import Header from "./components/Header"
+
 function App() {
 
   return (
-    <h1>
-      Home Spotify
-    </h1>
+    <Header/>
   )
 }
 
---
+++ src/App2.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const App2 = () => {
+    return <h1>"APP 2"</h1>
+}
+
+
+export default App2
