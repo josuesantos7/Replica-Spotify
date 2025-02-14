8f4c6cafa015ad88b96f98da7adb770cf74a9439
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2025-02-14 11:53:16 -0300

    Criado a base da página song e criado o componente Player.

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import Player from '../components/Player'
+import { Link } from 'react-router-dom'
 
 const Song = () => {
   return (
-    <div>
-      Song
+    <div className="song">
+      <div className="song__container">
+          <div className="song__image-container">
+            <img src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24" alt="Imagem da música x" />
+          </div>
+      </div>
+
+      <div className="song__bar">
+        <Link to="/artist/1" className='song__artist-image'>
+          <img 
+          width={75}
+          height={75}
+          src="https://i.scdn.co/image/ab676161000051744dcd8a3bff84cd7703892cf4" alt="imagem do artista Y" />
+        </Link>
+        
+        <Player/>
+
+        <div className='song__name'>
+          <p>Última Saudade - Ao Vivo</p>
+          <p>Henrique & Juliano</p>
+        </div>
+      </div>
+
     </div>
   )
 }
