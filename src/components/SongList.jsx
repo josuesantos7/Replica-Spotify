2f4fc92b5c24b2e81e0b5425727b11c5fb6fdd6a
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2025-02-14 16:16:29 -0300

    Realizado a Re-reinderização de componentes através do UseState para exibir mais músicas na página artist

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import SongItem from './SongItem'
+import { useState } from 'react';
 
 const SongList = ({ songsArray }) => {
-    const itens = 5;
+    const [itens, setItens] = useState(5);
 
     return <div className="song-list">
         {songsArray.filter((currentValue, index) => index < itens).map((currentSongObj, index) => <SongItem {...currentSongObj} index={index} key={index}/>)}
         
-        <p className='song-list__see-more'>Ver mais</p>
+        <p className='song-list__see-more' onClick={() => { setItens(itens + 5) }}>Ver mais</p>
   </div>
 }
 
