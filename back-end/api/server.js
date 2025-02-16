a43cfcc99d47ecd93270498046b1e18e9e706f27
josuesantos7 <110208995+josuesantos7@users.noreply.github.com>
2025-02-16 02:26:52 -0300

    Instalado o Axios e Cors, criado o arquivo api.js no Front  para chamar o Back-end e atualizado os arquivos artists.js e songs.js

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import cors from "cors";
 import { db } from './connect.js';
 
 
 const app = express();
 const PORT = 3002;
+
+app.use(cors())
 
 app.get("/artists", async(req, res) => {
     res.send(await db.collection("artists").find({}).toArray());
